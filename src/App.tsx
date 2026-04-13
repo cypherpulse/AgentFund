db638e9083c3d1ec3ae9733108dd598bb260c5fe
cypherpulse <johnbradill67@gmail.com>
2026-04-14 02:05:51 +0300

    initialize agent fund - src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { Web3Provider } from "@/components/Web3Provider";
+import Index from "./pages/Index.tsx";
+import NotFound from "./pages/NotFound.tsx";
+
+const App = () => (
+  <Web3Provider>
+    <TooltipProvider>
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </Web3Provider>
+);
+
+export default App;
